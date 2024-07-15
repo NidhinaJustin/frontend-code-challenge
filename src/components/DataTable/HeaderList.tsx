0b994862a9f0f37c835cf7a019dec1036309d01a
justinjames92 <nj9428947@gmail.com>
2024-07-14 23:39:07 -0400

    sort implemented for table headers

--- src/components/DataTable/HeaderList.tsx
+++ src/components/DataTable/HeaderList.tsx
@@ -8,54 +8,49 @@ interface HeaderListProps {
   isChangeTheme: boolean;
 }
 
-export default function HeaderList({
-  isChangeTheme,
-}: HeaderListProps) {
+export default function HeaderList({ isChangeTheme }: HeaderListProps) {
   const [isColumnSettingOpen, setIsColumnSettingOpen] =
     useState<boolean>(false);
-    const {
-        columnList,
-        setColumnList
-      } = useDataTableContext();
+  const { columnList, setColumnList } = useDataTableContext();
 
-    const handleCheckboxChange = (column: ColumnHeader) => {
-        let headerList = [...columnList];
-        let header = column;
-        header.isChecked = header.isChecked ? !header.isChecked : true;
-        const index = headerList.findIndex((item) => item.name === column.name);
-        if (index !== -1) {
-          headerList[index] = header;
-        }
-        setColumnList(headerList);
-      };
-
+  const handleCheckboxChange = (column: ColumnHeader) => {
+    let headerList = [...columnList];
+    let header = column;
+    header.isChecked = header.isChecked ? !header.isChecked : true;
+    const index = headerList.findIndex((item) => item.name === column.name);
+    if (index !== -1) {
+      headerList[index] = header;
+    }
+    setColumnList(headerList);
+  };
 
   const handleSelectHeaders = () => {
     setIsColumnSettingOpen(!isColumnSettingOpen);
   };
 
   return (
-    <div className="headerDropdown">
+    <div>
       <div title="select header" onClick={handleSelectHeaders}>
         <SettingsIcon style={{ color: isChangeTheme ? "white" : "#34495E" }} />
       </div>
-      {isColumnSettingOpen &&
-        columns.map((col, index) => (
-          <div key={index}>
-            <label>
-              <input
-                type="checkbox"
-                className="ml-2"
-                name={col.name}
-                checked={col["isChecked"] ?? false}
-                onChange={() => handleCheckboxChange(col)}
-              />
-              {col.name}
-            </label>
-          </div>
-        ))}
+      {isColumnSettingOpen && (
+        <div className="headerDropdown">
+          {columns.map((col, index) => (
+            <div key={index}>
+              <label>
+                <input
+                  type="checkbox"
+                  className="ml-2"
+                  name={col.name}
+                  checked={col["isChecked"] ?? false}
+                  onChange={() => handleCheckboxChange(col)}
+                />
+                {col.name}
+              </label>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
-
--- src/components/DataTable/TableContent.tsx
+++ src/components/DataTable/TableContent.tsx
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 import { useDataTableContext } from "../../DataTableContext";
 import { UserData } from "./DataTableTypes";
 
 export default function TableContent() {
-  const { headers, userData, setUserData, filteredData, isChangeTheme } =
+  const { headers, filteredData, isChangeTheme ,setFilteredData } =
     useDataTableContext();
+  const [order, setOrder] = useState("ASC");
 
-  function handleHeaderClick(header: string) {
-    let title = header as keyof UserData;
-    const newdata = [...userData].sort((a, b) =>
-      a[title] > b[title] ? 1 : -1
-    );
-    setUserData(newdata);
+  function handleHeaderClick(header: keyof UserData) {
+    const sortedData = [...filteredData].sort((a, b) => {
+      const aValue = a[header];
+      const bValue = b[header];
+      
+      // Handle string comparison
+      if (typeof aValue === "string" && typeof bValue === "string") {
+        return order === "ASC"
+          ? aValue.toLowerCase().localeCompare(bValue.toLowerCase())
+          : bValue.toLowerCase().localeCompare(aValue.toLowerCase());
+      }
+
+      // Handle number comparison
+      if (typeof aValue === "number" && typeof bValue === "number") {
+        return order === "ASC" ? aValue - bValue : bValue - aValue;
+      }
+
+      return 0; // Handle cases where values are not comparable like Undefined or Null Values or comparing different Types:
+    });
+
+    setFilteredData(sortedData);
+    setOrder(order === "ASC" ? "DSC" : "ASC");
   }
   const getKeyName = (label: string) => {
     let key = label?.toLowerCase();
     return key as keyof UserData;
   };
-  console.log(filteredData);
-  
   return (
     <div className="scrollable-table">
       {headers.length ? (
@@ -27,7 +42,7 @@ export default function TableContent() {
           <thead>
             <tr>
               {headers.map((col, key) => (
-                <th onClick={() => handleHeaderClick(col.name)} key={key}>
+                <th onClick={() => handleHeaderClick(getKeyName(col.name))} key={key}>
                   {col.name}
                 </th>
               ))}
